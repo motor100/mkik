70bf60ae7fb99a952ea9b7e3806fb2d643f30473
Evgeny <260443@mail.ru>
2023-11-10 15:51:21 +0500

    Login checkbox remember me

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,7 +26,7 @@
                 <!-- <x-label for="name" :value="__('Login')" /> -->
                 <x-label for="name" :value="__('Логин')" />
 
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+                <x-input id="name" class="block mt-1 w-full input-field" type="text" name="name" :value="old('name')" required autofocus />
             </div>
 
             <!-- Password -->
@@ -34,19 +34,19 @@
                 <!-- <x-label for="password" :value="__('Password')" /> -->
                 <x-label for="password" value="Пароль" />
 
-                <x-input id="password" class="block mt-1 w-full"
+                <x-input id="password" class="block mt-1 w-full input-field"
                                 type="password"
                                 name="password"
                                 required autocomplete="current-password" />
             </div>
 
             <!-- Remember Me -->
-            <!-- <div class="block mt-4">
+            <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Запомнить меня') }}</span>
                 </label>
-            </div> -->
+            </div>
 
             <div class="flex items-center justify-end mt-4">
             <!--
