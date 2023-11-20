faea67bccc100a974e693f3bb978104fdc359d2b
Evgeny <260443@mail.ru>
2023-11-20 21:36:46 +0500

    Some updates

--- resources/views/dshi-platnye-obrazovatelnye-uslugi.blade.php
+++ resources/views/dshi-platnye-obrazovatelnye-uslugi.blade.php
@@ -1,21 +1,15 @@
-@section('title', 'Корзина')
+@section('title', 'Платные образовательные услуги')
 
 @extends('layouts.main')
-
-@section('style')
-@endsection
 
 @section('content')
   <div class="page istoriya-page">
     <div class="container">
-      <div class="page-title">Корзина</div>
+      <div class="page-title">Платные образовательные услуги</div>
       <div class="text">
         {!! $text !!}
       </div>
       <div class="clear-both"></div>
     </div>
   </div>
-@endsection
-
-@section('script')
 @endsection
--- resources/views/dshi-postupayushchim.blade.php
+++ resources/views/dshi-postupayushchim.blade.php
@@ -1,21 +1,15 @@
-@section('title', 'Корзина')
+@section('title', 'Поступающим')
 
 @extends('layouts.main')
-
-@section('style')
-@endsection
 
 @section('content')
   <div class="page istoriya-page">
     <div class="container">
-      <div class="page-title">Корзина</div>
+      <div class="page-title">Поступающим</div>
       <div class="text">
         {!! $text !!}
       </div>
       <div class="clear-both"></div>
     </div>
   </div>
-@endsection
-
-@section('script')
 @endsection
